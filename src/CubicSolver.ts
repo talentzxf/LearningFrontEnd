a8e9122fd05c3ed69e02a4c0611b41aebeb59bd5
Zhang <vincentzhang@ea.com>
2017-01-03 08:10:23 +0800

    First commit of cubicSolver.

--- src/CubicSolver.ts
+++ src/CubicSolver.ts
@@ -1,3 +1,21 @@
+// TODO: Add param validations
+Array.prototype.subarray = function (start, end) {
+    if (end == null) {
+        end = this.length;
+    }
+
+    if (start < 0 || end > this.length) {
+        throw "Param error!"
+    }
+
+    let retArray = [];
+    for (let idx = start; idx <= end; idx++) {
+        retArray.push(this[idx]);
+    }
+
+    return retArray;
+}
+
 export class CubicSolver {
     affectedCubies:Array =
         [
@@ -10,6 +28,7 @@ export class CubicSolver {
         ];
 
     phase:number;
+    inputArray:Array;
 
     inverse(move:number) {
         return move + 2 - 2 * (move % 3);
@@ -39,38 +58,144 @@ export class CubicSolver {
 
     id(state:Array<number>) {
 
-    //--- Phase 1: Edge orientations.
-    if( this.phase < 2 )
-        return _.
-//    return vi( state.begin() + 20, state.begin() + 32 );
-//
-//    //-- Phase 2: Corner orientations, E slice edges.
-//    if( phase < 3 ){
-//    vi result( state.begin() + 31, state.begin() + 40 );
-//    for( int e=0; e<12; e++ )
-//    result[0] |= (state[e] / 8) << e;
-//    return result;
-//}
-//
-////--- Phase 3: Edge slices M and S, corner tetrads, overall parity.
-//if( phase < 4 ){
-//    vi result( 3 );
-//    for( int e=0; e<12; e++ )
-//    result[0] |= ((state[e] > 7) ? 2 : (state[e] & 1)) << (2*e);
-//    for( int c=0; c<8; c++ )
-//    result[1] |= ((state[c+12]-12) & 5) << (3*c);
-//    for( int i=12; i<20; i++ )
-//    for( int j=i+1; j<20; j++ )
-//    result[2] ^= state[i] > state[j];
-//    return result;
-//}
-//
-////--- Phase 4: The rest.
-//return state;
+        //--- Phase 1: Edge orientations.
+        if (this.phase < 2)
+            return state.subarray(20, 32);
+
+        //-- Phase 2: Corner orientations, E slice edges.
+        if (this.phase < 3) {
+            let result = state.subarray(31, 40);
+            for (let e = 0; e < 12; e++)
+                result[0] |= (state[e] / 8) << e;
+            return result;
+        }
+
+//--- Phase 3: Edge slices M and S, corner tetrads, overall parity.
+        if (this.phase < 4) {
+            let result = [];
+
+            for (let e = 0; e < 12; e++)
+                result[0] |= ((state[e] > 7) ? 2 : (state[e] & 1)) << (2 * e);
+            for (let c = 0; c < 8; c++)
+                result[1] |= ((state[c + 12] - 12) & 5) << (3 * c);
+            for (let i = 12; i < 20; i++)
+                for (let j = i + 1; j < 20; j++)
+                    result[2] ^= state[i] > state[j];
+            return result;
+        }
+
+//--- Phase 4: The rest.
+        return state;
+    }
+
+    solve(){
+        //--- Define the goal.
+        let goal = [ "UF", "UR", "UB", "UL", "DF", "DR", "DB", "DL", "FR", "FL", "BR", "BL",
+            "UFR", "URB", "UBL", "ULF", "DRF", "DFL", "DLB", "DBR" ];
+
+        //--- Prepare current (start) and goal state.
+        let currentState = [], goalState = [];
+        for( let i=0; i<20; i++ ){
+
+            //--- Goal state.
+            goalState[i] = i;
+
+            //--- Current (start) state.
+            let cubie = this.inputArray[i];
+            //while( (currentState[i] = find( goal, goal+20, cubie ) - goal) == 20){
+            //    cubie = cubie.substr( 1 ) + cubie[0];
+            //    currentState[i+20]++;
+            //}
+        }
+    //
+    //    //--- Dance the funky Thistlethwaite...
+    //    while( ++phase < 5 ){
+    //
+    //        //--- Compute ids for current and goal state, skip phase if equal.
+    //        vi currentId = id( currentState ), goalId = id( goalState );
+    //        if( currentId == goalId )
+    //            continue;
+    //
+    //        //--- Initialize the BFS queue.
+    //        queue<vi> q;
+    //        q.push( currentState );
+    //        q.push( goalState );
+    //
+    //        //--- Initialize the BFS tables.
+    //        map<vi,vi> predecessor;
+    //        map<vi,int> direction, lastMove;
+    //        direction[ currentId ] = 1;
+    //        direction[ goalId ] = 2;
+    //
+    //        //--- Dance the funky bidirectional BFS...
+    //        while( 1 ){
+    //
+    //            //--- Get state from queue, compute its ID and get its direction.
+    //            vi oldState = q.front();
+    //            q.pop();
+    //            vi oldId = id( oldState );
+    //            int& oldDir = direction[oldId];
+    //
+    //            //--- Apply all applicable moves to it and handle the new state.
+    //            for( int move=0; move<18; move++ ){
+    //                if( applicableMoves[phase] & (1 << move) ){
+    //
+    //                    //--- Apply the move.
+    //                    vi newState = applyMove( move, oldState );
+    //                    vi newId = id( newState );
+    //                    int& newDir = direction[newId];
+    //
+    //                    //--- Have we seen this state (id) from the other direction already?
+    //                    //--- I.e. have we found a connection?
+    //                    if( newDir  &&  newDir != oldDir ){
+    //
+    //                        //--- Make oldId represent the forwards and newId the backwards search state.
+    //                        if( oldDir > 1 ){
+    //                            swap( newId, oldId );
+    //                            move = inverse( move );
+    //                        }
+    //
+    //                        //--- Reconstruct the connecting algorithm.
+    //                        vi algorithm( 1, move );
+    //                        while( oldId != currentId ){
+    //                            algorithm.insert( algorithm.begin(), lastMove[ oldId ] );
+    //                            oldId = predecessor[ oldId ];
+    //                        }
+    //                        while( newId != goalId ){
+    //                            algorithm.push_back( inverse( lastMove[ newId ] ));
+    //                            newId = predecessor[ newId ];
+    //                        }
+    //
+    //                        //--- Print and apply the algorithm.
+    //                        for( int i=0; i<(int)algorithm.size(); i++ ){
+    //                            cout << "UDFBLR"[algorithm[i]/3] << algorithm[i]%3+1;
+    //                            currentState = applyMove( algorithm[i], currentState );
+    //                        }
+    //
+    //                        //--- Jump to the next phase.
+    //                        goto nextPhasePlease;
+    //                    }
+    //
+    //                    //--- If we've never seen this state (id) before, visit it.
+    //                    if( ! newDir ){
+    //                        q.push( newState );
+    //                        newDir = oldDir;
+    //                        lastMove[ newId ] = move;
+    //                        predecessor[ newId ] = oldId;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        nextPhasePlease:
+    //            ;
+    //    }
+    //}
     }
 
 
-    constructor(inputArray:Array<string>) {
 
+
+    constructor(inputArray:Array<string>) {
+        this.inputArray = inputArray;
     }
 }
