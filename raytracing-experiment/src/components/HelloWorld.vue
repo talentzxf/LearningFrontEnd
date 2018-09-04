5abf52aef9078c07bfa919edee6da4e9becb1fb3
Zhang <vincentzhang@ea.com>
2018-09-04 15:38:17 +0800

    Begin to write some simple ray-tracing apps

---
+++ raytracing-experiment/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="MainWindow">
+    <div id="glmaincanvas"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+    mounted: function () {
+      // Begin to render
+      var GL = require("litegl")
+      var gl = GL.create({width:800, height: 800})
+
+      document.getElementById("glmaincanvas").appendChild(gl.canvas)
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
