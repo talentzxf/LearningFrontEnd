2a086e84ca1cceb8cd72b2f0cab5bc20c5e62a3e
talentzxf <talentzxf@hotmail.com>
2018-10-09 15:31:05 +0800

    Continue working

--- raytracing-experiment/src/components/InvertedPendulum.vue
+++ raytracing-experiment/src/components/InvertedPendulum.vue
@@ -4,10 +4,23 @@
 
 <script>
     export default {
-        name: "InvertedPendulum"
+        name: "InvertedPendulum",
+        mounted: function(){
+          var scene = new THREE.Scene();
+          var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+          var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+          var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+          var cube = new THREE.Mesh( geometry, material );
+          scene.add( cube );
+
+          camera.position.z = 5;
+          var renderer = new THREE.WebGLRenderer();
+          renderer.setSize( window.innerWidth, window.innerHeight );
+          document.body.appendChild( renderer.domElement );
+        }
     }
 </script>
 
 <style scoped>
-
 </style>
